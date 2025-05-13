d6698b9294ebda793624a80894feb3d1101dc3bd
nivanyy <silva.nivany@escolar.ifrn.edu.br>
2025-05-13 14:07:10 -0300

    1113330

--- POOO/Assets/Movimentodoinimigo.cs
+++ POOO/Assets/Movimentodoinimigo.cs
@@ -4,29 +4,40 @@ public class Movimentodoinimigo : MonoBehaviour
 {
     private GameObject _player;
     
-    private Rigidbody _rigidBody;
+    private Rigidbody _rigidbody;
     private float velocidade;
 
-    public float realDivisao = 3;
+    public float raioDeVisao = 3;
+    private bool naVisao = false;
+    
+    private SphereCollider _sphereCollider;
     void Start()
     {
         velocidade = gameObject.GetComponent<inimigodoper>().Velocidade();
-        _rigidBody = gameObject.GetComponent<Rigidbody>();
-        
+        _rigidbody = gameObject.GetComponent<Rigidbody>();
+        _sphereCollider = gameObject.GetComponent<SphereCollider>();
         
         _player = GameObject.FindWithTag("Player");
     }
 
 
-
-
     void Update()
     {
-        if (Vector3.Distance(transform.position, _player.transform.position) < realDivisao)
-
+        if (Vector3.Distance(transform.position, _player.transform.position) < raioDeVisao)
         {
             transform.LookAt(_player.transform.position);
             transform.position = Vector3.MoveTowards(transform.position, _player.transform.position, velocidade * Time.deltaTime);
         }
     }
+
+    void OnTriggerStay(Collider colisao)
+    {
+        if (colisao.gameObject.CompareTag("Player"))
+        {
+            naVisao = true;
+        }
+    }
+
+   
 }
+ 
